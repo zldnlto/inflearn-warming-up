b678120e827ad966970016819fa2e8449c2e2d22
zldnlto <langchomi0@gmail.com>
2024-05-08 04:09:51 +0900

    M3_정답 선택하지 않고 Next 버튼 누르면 Life Point가 감소하고, 모든 Life Point 소진 시 해골 이모지 표시 #1

--- 03_퀴즈 앱/script.js
+++ 03_퀴즈 앱/script.js
@@ -14,23 +14,39 @@ answerBtnWrapper.classList.add("btn-wrapper");
 quizSection.appendChild(answerBtnWrapper);
 
 // option 버튼
-const optionBtn = document.createElement("button");
-optionBtn.classList.add("btn", "option-btn");
-optionBtn.innerText = "Next";
+const nextBtn = document.createElement("button");
+nextBtn.classList.add("btn", "option-btn");
+nextBtn.innerText = "Next";
 
-quizSection.append(optionBtn);
+// 재시작 버튼 hidden 디폴트임
+const restartBtn = document.createElement("button");
+restartBtn.classList.add("btn", "option-btn", "hidden");
+restartBtn.innerText = "Restart";
+
+quizSection.append(nextBtn, restartBtn);
 
 //기능
 
 // 도전횟수
 let LIFE_POINT = 2;
 
+const activeRestartBtn = () => {
+  lifePoint.innerText = "☠️";
+  nextBtn.classList.add("hidden");
+  restartBtn.classList.remove("hidden");
+};
+
 const drawLifePointEmoji = (LP) => {
+  console.log(LP);
   let lifePointEmoji = "";
-  for (let i = 0; i < LP; i++) {
-    lifePointEmoji += "❤️ ";
+  if (LP === 0) {
+    activeRestartBtn();
+  } else {
+    for (let i = 0; i < LP; i++) {
+      lifePointEmoji += "❤️ ";
+    }
+    lifePoint.innerText = lifePointEmoji;
   }
-  lifePoint.innerText = lifePointEmoji;
   return lifePointEmoji;
 };
 
@@ -107,6 +123,7 @@ const handleAnswerBtn = (e) => {
     container.classList.add("correct");
     e.target.classList.add("correct");
   } else {
+    // 오답 경우
     container.classList.add("incorrect");
     e.target.classList.add("incorrect");
 
@@ -114,10 +131,10 @@ const handleAnswerBtn = (e) => {
       (v) => v.id === "correct"
     );
     correctAnswer.classList.add("correct");
+    //얘랑 중복코드
     LIFE_POINT = LIFE_POINT - 1;
     drawLifePointEmoji(LIFE_POINT);
   }
-  console.log(Array.from(answerBtnWrapper.children));
   const btns = Array.from(answerBtnWrapper.children);
   btns.forEach((btn) => {
     if (!(btn.id || btn.classList.contains("incorrect"))) {
@@ -127,6 +144,17 @@ const handleAnswerBtn = (e) => {
 };
 
 const handleNextBtn = () => {
+  // 정답 안 고르고 Next 선택해도 생명 까임
+  const btns = Array.from(answerBtnWrapper.children);
+
+  const isSelectedBtn = btns.filter(
+    (v) => v.classList.contains("incorrect") || v.classList.contains("correct")
+  );
+  if (isSelectedBtn.length === 0) {
+    LIFE_POINT = LIFE_POINT - 1;
+    drawLifePointEmoji(LIFE_POINT);
+  }
+
   console.log("Next");
   while (answerBtnWrapper.firstChild) {
     answerBtnWrapper.removeChild(answerBtnWrapper.firstChild);
@@ -135,7 +163,7 @@ const handleNextBtn = () => {
   displayQuizData(quizData);
 };
 
-optionBtn.addEventListener("click", handleNextBtn);
+nextBtn.addEventListener("click", handleNextBtn);
 
 // Next까지 구현 완료, 정답이나 오답 선택하고 나서 다른 버튼 비활성화 시키기 ..
 // 문제은행중에 해당 문제 제거 기능 (선택)
