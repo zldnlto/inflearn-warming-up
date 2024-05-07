5f4d9a6f6d7439657acca7d4bbc398876023215d
zldnlto <langchomi0@gmail.com>
2024-05-08 03:01:38 +0900

    M3_버튼 정답 오답 처리 이벤트 핸들러 구현

--- 03_퀴즈 앱/script.js
+++ 03_퀴즈 앱/script.js
@@ -1,5 +1,7 @@
 // DOM
 
+const container = document.querySelector(".container");
+
 const quizSection = document.querySelector(".quiz-wrapper");
 
 const questionText = document.createElement("p");
@@ -9,19 +11,6 @@ quizSection.appendChild(questionText);
 const answerBtnWrapper = document.createElement("div");
 answerBtnWrapper.classList.add("btn-wrapper");
 
-// const answerBtn1 = document.createElement("button");
-// answerBtn1.classList.add("btn", "answer-btn");
-// answerBtn1.innerText = "선택지1"; // 수정
-
-// const answerBtn2 = document.createElement("button");
-// answerBtn2.classList.add("btn", "answer-btn");
-// answerBtn2.innerText = "선택지2"; //수정
-
-// const answerBtn3 = document.createElement("button");
-// answerBtn3.classList.add("btn", "answer-btn");
-// answerBtn3.innerText = "선택지3"; //수정
-
-// answerBtnWrapper.append(answerBtn1, answerBtn2, answerBtn3);
 quizSection.appendChild(answerBtnWrapper);
 
 // option 버튼
@@ -32,9 +21,6 @@ optionBtn.innerText = "Next";
 quizSection.append(optionBtn);
 
 //기능
-
-// data 랜덤으로 불러와서 문제, 선택지에 뿌려줌 (랜덤으로)
-// 오답 생성은 어떻게 ?
 
 const LIFE_POINT = 2; // 도전횟수
 
@@ -92,11 +78,33 @@ const addAnswerBtn = (quizItem) => {
     const btn = document.createElement("button");
     btn.classList.add("btn", "answer-btn");
     if (index === 0) {
-      btn.id === "correct";
+      btn.id = "correct";
     }
-    btn.innerText = item; // 수정
+    btn.addEventListener("click", handleAnswerBtn);
+    btn.innerText = item;
     btnArr.push(btn);
   });
   shuffle(btnArr);
   answerBtnWrapper.append(...btnArr);
 };
+
+handleAnswerBtn = (e) => {
+  if (e.target.id === "correct") {
+    container.classList.add("correct");
+    e.target.classList.add("correct");
+  } else {
+    container.classList.add("incorrect");
+    e.target.classList.add("incorrect");
+
+    const correctAnswer = Array.from(answerBtnWrapper.children).find(
+      (v) => v.id === "correct"
+    );
+    correctAnswer.classList.add("correct");
+  }
+};
+
+handleNextBtn = () => {
+  console.log("Next");
+};
+
+optionBtn.addEventListener("click", handleNextBtn);
