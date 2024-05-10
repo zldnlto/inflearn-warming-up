875a7c197f35708e6f0f9b64a4b24da86878aaf0
zldnlto <langchomi0@gmail.com>
2024-05-10 16:23:49 +0900

    M4_책 추가 기능 구현 #5

--- 04_책 리스트 앱/script.js
+++ 04_책 리스트 앱/script.js
@@ -16,25 +16,68 @@ const bookListSection = main.querySelector("#book-list-section");
 
 const bookListItems = bookListSection.querySelector(".book-list-items");
 
-// item DOM 생성
-const bookListItem = document.createElement("li");
-bookListItem.className = "book-list-item";
-const bookTitle = document.createElement("span");
-bookTitle.className = "title-item";
-bookTitle.innerText = "책이름1";
+const generateBookListItem = (bookInfo) => {
+  console.log(bookInfo);
+  const bookListItem = document.createElement("li");
+  bookListItem.className = "book-list-item";
+  const bookTitle = document.createElement("span");
+  bookTitle.className = "title-item";
+  bookTitle.innerText = bookInfo.title;
 
-const bookAuthor = document.createElement("span");
-bookAuthor.className = "author-item";
-bookAuthor.innerText = "저자명";
+  const bookAuthor = document.createElement("span");
+  bookAuthor.className = "author-item";
+  bookAuthor.innerText = bookInfo.author;
 
-const bookItemDeleteBtnBox = document.createElement("span");
-const bookItemDeleteBtn = document.createElement("button");
-bookItemDeleteBtn.className = "delete-btn";
-bookItemDeleteBtnBox.appendChild(bookItemDeleteBtn);
+  const bookItemDeleteBtnBox = document.createElement("span");
+  const bookItemDeleteBtn = document.createElement("button");
+  bookItemDeleteBtn.className = "delete-btn";
+  bookItemDeleteBtnBox.appendChild(bookItemDeleteBtn);
 
-bookListItem.append(bookTitle, bookAuthor, bookItemDeleteBtnBox);
+  //deleteBtn 이벤트리스너 달기
 
-bookListItems.append(bookListItem);
+  bookListItem.append(bookTitle, bookAuthor, bookItemDeleteBtnBox);
+
+  return bookListItem;
+  //append는 따로해야함   bookListItems.append(bookListItem);
+};
+
+let bookInfo = { title: "", author: "" };
+
+const handleTitleInput = (e) => {
+  bookInfo.title = e.target.value;
+};
+
+const handleAuthorInput = (e) => {
+  bookInfo.author = e.target.value;
+};
+
+titleInput.addEventListener("keydown", handleTitleInput);
+
+authorInput.addEventListener("keydown", handleAuthorInput);
+
+const addBookItem = (data) => {
+  console.log("addBookItem");
+  const bookItem = generateBookListItem(data);
+  bookListItems.append(bookItem);
+  //구현
+};
+
+//추가할 때 notice 출력
+//추가하고 나서 form 비우기
+
+const handleSubmitBtn = (e) => {
+  e.preventDefault();
+  addBookItem(bookInfo);
+
+  //비우기 작업
+  titleInput.value = "";
+  authorInput.value = "";
+  bookInfo.title = "";
+  bookInfo.author = "";
+  console.log(bookInfo);
+};
+
+submitBtn.addEventListener("click", handleSubmitBtn);
 
 //삭제
 //삭제할 때 notice 출력
