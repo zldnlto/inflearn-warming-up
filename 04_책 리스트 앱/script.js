637977d75dc157ff0b82353238cb7ed563050485
zldnlto <langchomi0@gmail.com>
2024-05-10 15:57:59 +0900

    M4_DOM 집어오기

---
+++ 04_책 리스트 앱/script.js
@@ -0,0 +1,23 @@
+// DOM
+
+const main = document.getElementById("main");
+
+//FORM_SECTION
+
+const formSection = main.querySelector("#form-section");
+const bookForm = formSection.querySelector(".book-form");
+const titleInput = bookForm.querySelector(".title-input");
+const authorInput = bookForm.querySelector(".author-input");
+
+const submitBtn = bookForm.querySelector(".submit-btn");
+
+// BOOK_LIST_SECTION
+const bookListSection = main.querySelector("#form-section");
+
+//추가하기
+
+//추가할 때 notice 출력
+//추가하고 나서 form 비우기
+
+//삭제
+//삭제할 때 notice 출력
