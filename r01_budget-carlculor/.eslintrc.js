e57d8797c29272919c9f17aa8a9f72d30e15eb96
zldnlto <langchomi0@gmail.com>
2024-05-15 01:53:07 +0900

    r01_eslint-plugin-tailwindcss 설치 #10

---
+++ r01_budget-carlculor/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: ["plugin:tailwindcss/recommended"],
+
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx", "*.js"],
+      parser: "@typescript-eslint/parser",
+    },
+  ],
+};
