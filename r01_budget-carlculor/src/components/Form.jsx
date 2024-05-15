8ed73783117a4c8550547e35ea16c14ee9ea074f
zldnlto <langchomi0@gmail.com>
2024-05-16 01:07:40 +0900

    r01_Form 컴포넌트 기능 구현 #10

--- r01_budget-carlculor/src/components/Form.jsx
+++ r01_budget-carlculor/src/components/Form.jsx
@@ -1,8 +1,24 @@
-
-import React from "react";
+import React, { useCallback } from "react";
 import { icons } from "./svg";
 
-function Form() {
+function Form({ setData, onSubmit }) {
+  const handleNameChange = useCallback((e) => {
+    const newName = e.target.value;
+    setData((prev) => ({
+      ...prev,
+      name: newName,
+    }));
+  }, []);
+
+  // useRef로 리팩터링해서 DOM 직접 조작 -> focus시에 ""으로
+  const handleCostChange = useCallback((e) => {
+    const newCost = e.target.value.replace(/^0+/, "");
+    setData((prev) => ({
+      ...prev,
+      cost: newCost,
+    }));
+  }, []);
+
   return (
     <form className="sec-style w-4/5 min-w-max">
       <fieldset className="flex justify-between gap-x-2">
@@ -11,16 +27,28 @@ function Form() {
           <label htmlFor="category" className="text-green-800">
             지출 항목
           </label>
-          <input type="text" id="category" placeholder="예) 렌트비" className="input-style" />
+          <input
+            type="text"
+            id="category"
+            placeholder="예) 렌트비"
+            className="input-style"
+            onChange={handleNameChange}
+          />
         </div>
         <div className="flex w-1/2 flex-col">
           <label htmlFor="cost" className="text-green-800">
             비용
           </label>
-          <input type="number" id="cost" defaultValue={0} className="input-style " />
+          <input
+            type="number"
+            id="cost"
+            defaultValue={0}
+            className="input-style"
+            onChange={handleCostChange}
+          />
         </div>
       </fieldset>
-      <button type="submit" className="btn-with-icon  mt-2">
+      <button type="submit" className="btn-with-icon mt-4" onClick={onSubmit}>
         <span className="mt-0.5">{icons.submit}</span>
         <span>제출</span>
       </button>
@@ -28,4 +56,4 @@ function Form() {
   );
 }
 
-export default Form
+export default Form;
--- r01_budget-carlculor/src/components/List.jsx
+++ r01_budget-carlculor/src/components/List.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { icons } from "./svg";
 
 function List() {
-  return <div>List</div>;
+  return (
+    <div className="sec-style relative mt-3 w-4/5 min-w-max">
+      <button type="button" className="btn-with-icon">
+        <span className="mt-1">{icons.pen}</span>
+        <span>수정</span>
+      </button>
+      <ul className="flex flex-col gap-2">
+        <li className="item-style">
+          <span>교통비</span>
+          <span className="text-center">400</span>
+          <div className="flex gap-3">
+            <button type="button">{icons.pen}</button>
+            <button type="button"> {icons.trash}</button>
+          </div>
+        </li>
+        <li className="item-style">
+          <span>식비</span>
+          <span className="text-center">1200</span>
+          <div className="flex gap-3">
+            <button type="button">{icons.pen}</button>
+            <button type="button">{icons.trash}</button>
+          </div>
+        </li>
+      </ul>
+      <button type="button" className="btn-with-icon mt-4">
+        <span className="mt-0.5">{icons.trash}</span>
+        <span>목록 지우기</span>
+      </button>
+      <span className="absolute -bottom-8 right-1 flex gap-2">
+        <span className="mt-1">{icons.wallet}</span>
+        <span>총지출 : 1600원</span>
+      </span>
+    </div>
+  );
 }
 
 export default List;
