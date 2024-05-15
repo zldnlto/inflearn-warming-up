4baa57fb54889fbbc503d72da2690e948ad308ae
zldnlto <langchomi0@gmail.com>
2024-05-16 01:07:40 +0900

    r01_Form 컴포넌트 기능 구현 #10

--- r01_budget-carlculor/src/App.js
+++ r01_budget-carlculor/src/App.js
@@ -1,48 +1,26 @@
-import React from 'react';
-import Form from './components/Form';
-import { icons } from './components/svg';
+import React, { useEffect, useState } from "react";
+import Form from "./components/Form";
+import List from "./components/List";
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    console.log("모니터링", data);
+  }, [data]);
+
+  const handleSubmitButton = (e) => {
+    e.preventDefault();
+    console.log(data);
+    console.log("하이");
+  };
+
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center rounded border ">
-      {/* <p>아이템이 삭제되었습니다.</p> */}
+    <main className="flex h-screen w-screen flex-col items-center justify-center rounded border ">
       <h1 className="text-3xl">예산 계산기</h1>
-      <Form />
-      <div className="sec-style relative mt-3 w-4/5 min-w-max">
-        <button type="button" className="btn-with-icon">
-          <span className="mt-1">{icons.pen}</span>
-          <span>수정</span>
-        </button>
-        {/* ul에 font-bold 와 order 삭제  */}
-        <ul className="flex flex-col gap-2">
-          <li className="item-style">
-            <span>교통비</span>
-            <span className="text-center">400</span>
-            <div className="flex gap-3">
-              <button type="button">{icons.pen}</button>
-              <button type="button"> {icons.trash}</button>
-            </div>
-          </li>
-          <div className="mx-3 flex border text-center">하잇</div>
-          <li className="item-style">
-            <span>식비</span>
-            <span className="text-center">1200</span>
-            <div className="flex gap-3">
-              <button type="button">{icons.pen}</button>
-              <button type="button">{icons.trash}</button>
-            </div>
-          </li>
-        </ul>
-        <button type="button" className="btn-with-icon mt-2">
-          <span className="mt-0.5">{icons.trash}</span>
-          <span>목록 지우기</span>
-        </button>
-        <span className="absolute -bottom-8 right-1 flex gap-2">
-          <span className="mt-1">{icons.wallet}</span>
-          <span>총지출 : 1600원</span>
-        </span>
-      </div>
-    </div>
+      <Form setData={setData} onSubmit={handleSubmitButton} />
+      <List data={data} />
+    </main>
   );
 }
 
