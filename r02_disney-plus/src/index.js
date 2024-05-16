2729dabad4cd0916a72a40f59d656907e271bf36
zldnlto <langchomi0@gmail.com>
2024-05-16 21:19:49 +0900

    r02_recoil init #14

--- r02_disney-plus/src/index.js
+++ r02_disney-plus/src/index.js
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import AppRouter from "./router/AppRouter";
+import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppRouter />
+    <RecoilRoot>
+      <AppRouter />
+    </RecoilRoot>
   </React.StrictMode>
 );
 
