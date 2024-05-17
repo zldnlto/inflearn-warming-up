b2d2689bc7a6d651b4ea1cf4580fb83be9c4f4b7
zldnlto <langchomi0@gmail.com>
2024-05-18 02:45:41 +0900

    r02_메인 배너 기능 구현 #14

--- r02_disney-plus/src/components/main/Banner.jsx
+++ r02_disney-plus/src/components/main/Banner.jsx
@@ -1,23 +1,38 @@
 import React from "react";
 import Button from "../common/Button";
+import axios from "axios";
 
-function Banner() {
+const IMG_BASE_URL = "https://image.tmdb.org/t/p/original/";
+
+function Banner({ data }) {
+  console.log(data, "banner");
+
+  const imageSrc = `${IMG_BASE_URL}${data.backdrop_path}`;
+
+  console.log(imageSrc);
+
   return (
     <section className="h-screen">
-      <article className="flex h-4/5 bg-blue-950 text-white">
-        <div className="flex flex-col justify-center gap-8 px-8">
-          <h2 className="text-4xl font-bold ">위시</h2>
-          <button className="max-w-20 rounded bg-white px-3 py-2 text-sm text-black">
-            PLAY
-          </button>
-          <p className="w-1/3">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis
-            molestiae eius, et nostrum deserunt quia, saepe error alias
-            voluptates quisquam voluptas neque quibusdam, aut nisi esse! Ipsa
-            provident dolor voluptate.
-          </p>
-        </div>
-      </article>
+      <div className="relative h-full">
+        <article
+          className="box-shadow relative flex h-4/5 bg-cover text-white"
+          style={{ backgroundImage: `url(${imageSrc})` }}
+        >
+          <div className="z-10 flex flex-col justify-center gap-8 px-8">
+            <div>
+              <h2 className="text-4xl font-bold">{data.title}</h2>
+              <p className="text-2xl font-bold">{data.original_title}</p>
+            </div>
+            <button className="max-w-20 rounded bg-white px-3 py-2 text-sm text-black">
+              PLAY
+            </button>
+            <p className="text-overflow-banner w-1/4 min-w-72 text-balance">
+              {data.overview}
+            </p>
+          </div>
+        </article>
+        <div className="inset-shadow absolute inset-0"></div>
+      </div>
     </section>
   );
 }
--- r02_disney-plus/src/pages/Main/index.jsx
+++ r02_disney-plus/src/pages/Main/index.jsx
@@ -1,20 +1,21 @@
 import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import Banner from "../../components/main/Banner";
 import useMovieData from "../../utils/useMovieData";
+import getRandomElement from "../../utils/getRandomElement";
 
 function Main() {
   const trendingData = useMovieData("trendingNow");
   const nowPlayingData = useMovieData("nowPlaying");
 
   console.log(trendingData, "트렌드");
-
   console.log(nowPlayingData, "데이터 확인");
+  const trendingNow = getRandomElement(trendingData);
 
   return (
-    <div className="px-8">
-      <Banner />
-    </div>
+    <Suspense fallback={<div>Loading</div>}>
+      <div className="px-8">{trendingNow && <Banner data={trendingNow} />}</div>
+    </Suspense>
   );
 }
 
